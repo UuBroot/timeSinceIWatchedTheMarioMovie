06c44308895d849fa431e4cde9f3cff4688299c4
UuBroot <lolylyt@gmail.com>
2023-05-10 18:23:30 +0200

    i dont know

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ function doStuff(){
     console.log(unix_timestamp)
     // Create a new JavaScript Date object based on the timestamp
     // multiplied by 1000 so that the argument is in milliseconds, not seconds.
-    var date = new Date(unix_timestamp * 1000);
+    var date = new Date();
+    // Day of the week
+    var days = date.getDay();
     // Hours part from the timestamp
     var hours = date.getHours();
     // Minutes part from the timestamp
@@ -13,10 +15,10 @@ function doStuff(){
     var seconds = "0" + date.getSeconds();
 
     // Will display time in 10:30:23 format
-    var formattedTime = hours + 'h ' + minutes.substr(-2) + 'min ' + seconds.substr(-2) + 'sec';
-
+    //var formattedTime = days + 'd ' + hours + 'h ' + minutes.substr(-2) + 'min ' + seconds.substr(-2) + 'sec';
+    formattedTime = unix_timestamp;
     document.getElementById("thetime").innerHTML = formattedTime;
 }
 setInterval(function(){
     doStuff();
-},1000)
+},1000);
